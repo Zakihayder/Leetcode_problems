5e70924f7e2bb59fa1bffd17023fa8eb5626f036
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-08 07:03:17 -0700

    Time: 0 ms (100.00%) | Memory: 12.4 MB (79.76%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        
+        if needle not in haystack:
+            return -1
+        else:
+            return haystack.find(needle)
+        
