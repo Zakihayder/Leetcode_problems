1acb8eb88593d775ffbbe123f0fc83c93f1a7fd7
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-12 13:16:22 -0700

    Time: 4 ms (2.67%) | Memory: 12.9 MB (65.14%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        
+        for i in range(len(nums)):
+            if target == nums[i] or target < nums[i]:
+                return i
+        return len(nums)
+        
