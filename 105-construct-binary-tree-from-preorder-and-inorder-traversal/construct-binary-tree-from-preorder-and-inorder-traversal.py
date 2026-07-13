cefa3c9ce626766cadecc930c50a64571848cb59
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-13 05:18:11 -0700

    Time: 7 ms (77.68%) | Memory: 17.1 MB (56.64%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def buildTree(self, preorder, inorder):
+        # map each value to its index in inorder, for O(1) lookups
+        inorder_index = {val: i for i, val in enumerate(inorder)}
+        self.pre_idx = 0  
+
+        def build(left, right):
+            if left > right:
+                return None
+
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+
+            # find where root splits the inorder range
+            mid = inorder_index[root_val]
+
+
+            root.left = build(left, mid - 1)
+            root.right = build(mid + 1, right)
+
+            return root
+
+        return build(0, len(inorder) - 1)
