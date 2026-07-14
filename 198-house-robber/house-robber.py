6d85312efb4d6b3c94f8d85741486ce91064f8e9
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-14 02:16:04 -0700

    Time: 0 ms (100.00%) | Memory: 12.3 MB (56.88%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,13 +1,6 @@
 class Solution(object):
     def rob(self, nums):
         
-        if len(nums) == 3:
-            return max(nums[0]+nums[2],nums[1])
-        elif len(nums) == 2:
-            return max(nums[0],nums[1])
-        elif len(nums) == 3:
-            return nums[0]
-        else:
             prev1, prev2 = 0 ,0 
 
             for num in nums:
