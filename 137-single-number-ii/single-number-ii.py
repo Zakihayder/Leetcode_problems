517590b67b6248c30e196348beceee6781756174
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-15 04:07:25 -0700

    Time: 2 ms (72.58%) | Memory: 13.6 MB (35.10%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        nums.sort()
+
+        for i in range(0,len(nums)-2,3):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+
+        return nums[len(nums)-1]
+            
+        
