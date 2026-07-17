ced562f0ca07f9110c3bbd62ef11864895f8f8d9
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-17 10:15:09 -0700

    Time: 1015 ms (39.78%) | Memory: 35.8 MB (17.67%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/find-median-from-data-stream.py
@@ -1,17 +1,23 @@
+import heapq
+
 class MedianFinder(object):
 
     def __init__(self):
-        self.arr = []
+        self.small = []  # max-heap (stored as negatives, since Python only has min-heap)
+        self.large = []  # min-heap
 
     def addNum(self, num):
-        self.arr.append(num)
+        # push to max-heap first (negate to simulate max-heap using heapq's min-heap)
+        heapq.heappush(self.small, -num)
+
+        # ensure every element in `small` is <= every element in `large`
+        heapq.heappush(self.large, -heapq.heappop(self.small))
+
+        # rebalance sizes: `small` can have at most one more element than `large`
+        if len(self.large) > len(self.small):
+            heapq.heappush(self.small, -heapq.heappop(self.large))
 
     def findMedian(self):
-        self.arr.sort()  
-        n = len(self.arr)
-        mid = n // 2
-
-        if n % 2 == 1:
-            return self.arr[mid]  # odd length: middle element
-        else:
-            return (self.arr[mid - 1] + self.arr[mid]) / 2.0  # even length: average of two middle elements
+        if len(self.small) > len(self.large):
+            return -self.small[0]  # odd total count: middle element is top of `small`
+        return (-self.small[0] + self.large[0]) / 2.0  # even: average of both tops
