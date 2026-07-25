42b3479175fdd8f51a54924ab8b1294d9d108bb3
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-25 14:15:32 +0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.44%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -4,6 +4,7 @@ class Solution(object):
         while n > 0:
             arr.append(n%10)
             n = n//10
-        arr.sort()
-        return arr[len(arr)-1]*arr[len(arr)-2]
+        maximum = max(arr)
+        arr.remove(maximum)
+        return maximum * max(arr)
         
