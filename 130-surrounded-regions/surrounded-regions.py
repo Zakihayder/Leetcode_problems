546e5b0cc05f51a9a6f4928802d450b00fca8043
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-27 14:27:54 +0500

    Time: 11 ms (35.30%) | Memory: 17.1 MB (13.99%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def solve(self, board):
+        if not board or not board[0]:
+            return
+
+        rows, cols = len(board), len(board[0])
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != 'O':
+                return
+
+            board[r][c] = 'T'
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        # First and last column
+        for r in range(rows):
+            if board[r][0] == 'O':
+                dfs(r, 0)
+            if board[r][cols - 1] == 'O':
+                dfs(r, cols - 1)
+
+        # First and last row
+        for c in range(cols):
+            if board[0][c] == 'O':
+                dfs(0, c)
+            if board[rows - 1][c] == 'O':
+                dfs(rows - 1, c)
+
+        # Flip surrounded regions and restore border-connected ones
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == 'O':
+                    board[r][c] = 'X'
+                elif board[r][c] == 'T':
+                    board[r][c] = 'O'
