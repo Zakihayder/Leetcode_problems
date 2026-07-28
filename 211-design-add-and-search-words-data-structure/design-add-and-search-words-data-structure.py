b17afa16c36cd619550cbc0b7e48388b356e9e8a
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-07-28 22:46:01 +0500

    Time: 2703 ms (59.89%) | Memory: 114.8 MB (74.39%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -0,0 +1,41 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+
+
+class WordDictionary(object):
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        node = self.root
+
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+
+        node.isEnd = True
+
+    def search(self, word):
+
+        def dfs(node, i):
+            if i == len(word):
+                return node.isEnd
+
+            ch = word[i]
+
+            if ch == ".":
+                for child in node.children.values():
+                    if dfs(child, i + 1):
+                        return True
+                return False
+
+            if ch not in node.children:
+                return False
+
+            return dfs(node.children[ch], i + 1)
+
+        return dfs(self.root, 0)
