9cd8eb2be82a73c3798e75cd604cc01f3996409f
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-08-05 13:56:02 +0500

    Time: 258 ms (78.79%) | Memory: 145.5 MB (34.85%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.py
+++ 3561-remove-methods-from-project/remove-methods-from-project.py
@@ -1,34 +1,22 @@
 class Solution(object):
     def remainingMethods(self, n, k, invocations):
         graph = [[] for _ in range(n)]
-        undirected = [[] for _ in range(n)]
 
         for u, v in invocations:
             graph[u].append(v)
-            undirected[u].append(v)
-            undirected[v].append(u)
 
         suspicious = [False] * n
 
-        def dfs(node):
-            suspicious[node] = True
-            for nei in graph[node]:
-                if not suspicious[nei]:
-                    dfs(nei)
+        def dfs(u):
+            suspicious[u] = True
+            for v in graph[u]:
+                if not suspicious[v]:
+                    dfs(v)
 
         dfs(k)
 
-        visited = [False] * n
-
-        def dfs2(node):
-            visited[node] = True
-            for nei in undirected[node]:
-                if not visited[nei]:
-                    suspicious[nei] = False
-                    dfs2(nei)
-
-        for i in range(n):
-            if not suspicious[i] and not visited[i]:
-                dfs2(i)
+        for u, v in invocations:
+            if not suspicious[u] and suspicious[v]:
+                return list(range(n))
 
         return [i for i in range(n) if not suspicious[i]]
