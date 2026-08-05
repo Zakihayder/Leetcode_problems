89815b88ece3ae0090acbb839f55f780be13a3bf
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-08-05 23:27:53 +0500

    Time: 234 ms (13.28%) | Memory: 42.3 MB (87.23%) - LeetSync

---
+++ 2636-maximum-subsequence-score/maximum-subsequence-score.py
@@ -0,0 +1,21 @@
+import heapq
+
+class Solution(object):
+    def maxScore(self, nums1, nums2, k):
+        pairs = sorted(zip(nums2, nums1), reverse=True)
+
+        heap = []
+        total = 0
+        ans = 0
+
+        for n2, n1 in pairs:
+            heapq.heappush(heap, n1)
+            total += n1
+
+            if len(heap) > k:
+                total -= heapq.heappop(heap)
+
+            if len(heap) == k:
+                ans = max(ans, total * n2)
+
+        return ans
