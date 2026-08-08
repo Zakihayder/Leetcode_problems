6c27802904303eb23929c95793535fd7661c0ccc
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-08-08 14:53:54 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (64.11%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.py
+++ 60-permutation-sequence/permutation-sequence.py
@@ -1,19 +1,18 @@
 class Solution(object):
     def getPermutation(self, n, k):
         nums = list(range(1, n + 1))
+        fact = [1] * (n + 1)
+
+        for i in range(1, n + 1):
+            fact[i] = fact[i - 1] * i
+
+        k -= 1
         ans = []
 
-        k -= 1  # Convert k to 0-based index
-
         for i in range(n, 0, -1):
-            fact = 1
+            idx = k // fact[i - 1]
+            k %= fact[i - 1]
 
-            for j in range(1, i):
-                fact *= j
-
-            index = k // fact
-            k %= fact
-
-            ans.append(str(nums.pop(index)))
+            ans.append(str(nums.pop(idx)))
 
         return ''.join(ans)
