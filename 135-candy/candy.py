17c56553dd45eeb9b4de07f0cc49f431759c8d65
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-08-15 23:01:50 +0500

    Time: 55 ms (5.07%) | Memory: 25.3 MB (5.05%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -1,15 +1,43 @@
 class Solution:
-    def candy(self, ratings: List[int]) -> int:
+    def candy(self, ratings):
         n = len(ratings)
 
-        candies = [1] * n
+        if n <= 1:
+            return n
+
+        def triangle(x):
+            return x * (x + 1) // 2
+
+        ans = 0
+        up = 0
+        down = 0
+        prev_slope = 0
 
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
-                candies[i] = candies[i - 1] + 1
+                slope = 1
+            elif ratings[i] < ratings[i - 1]:
+                slope = -1
+            else:
+                slope = 0
 
-        for i in range(n - 2, -1, -1):
-            if ratings[i] > ratings[i + 1]:
-                candies[i] = max(candies[i], candies[i + 1] + 1)
+            if (prev_slope > 0 and slope == 0) or \
+               (prev_slope < 0 and slope >= 0):
 
-        return sum(candies)
+                ans += triangle(up) + triangle(down) + max(up, down)
+
+                up = 0
+                down = 0
+
+            if slope > 0:
+                up += 1
+            elif slope < 0:
+                down += 1
+            else:
+                ans += 1
+
+            prev_slope = slope
+
+        ans += triangle(up) + triangle(down) + max(up, down) + 1
+
+        return ans
