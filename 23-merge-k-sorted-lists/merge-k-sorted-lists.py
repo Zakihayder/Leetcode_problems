97ff42c67549b867cd1f7d3ef89fdd4a0656f5a6
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-08-29 15:56:14 +0500

    Time: 5 ms (88.74%) | Memory: 23.1 MB (29.09%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,20 +1,9 @@
 import heapq
 
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
 
-        # seed the heap with the head of each non-empty list
-        # include index i as a tie-breaker since ListNode isn't directly comparable
         for i, node in enumerate(lists):
             if node:
                 heapq.heappush(heap, (node.val, i, node))
@@ -24,10 +13,14 @@ class Solution(object):
 
         while heap:
             val, i, node = heapq.heappop(heap)
+
             curr.next = node
             curr = curr.next
 
             if node.next:
-                heapq.heappush(heap, (node.next.val, i, node.next))
+                heapq.heappush(
+                    heap,
+                    (node.next.val, i, node.next)
+                )
 
         return dummy.next
