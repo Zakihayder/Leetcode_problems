1159f125bdef5163ffc5ccc8106b1df64eba81aa
Zaki Haider <168919940+Zakihayder@users.noreply.github.com>
2026-08-30 18:29:50 +0500

    Time: 59 ms (10.33%) | Memory: 25.6 MB (17.96%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(ratings)
 
         candies = [1] * n
-
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = candies[i - 1] + 1
